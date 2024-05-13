c5e90f3d85adb69ff93d49cb3c0fe877f365f52a
archiiive <githubarchive10@gmail.com>
2024-05-13 13:10:41 +0900

    ⚡️ 24/5/13/4c

--- src/main/java/com/example/api01/security/filter/RefreshTokenFilter.java
+++ src/main/java/com/example/api01/security/filter/RefreshTokenFilter.java
@@ -16,6 +16,8 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.time.Instant;
+import java.util.Date;
 import java.util.Map;
 
 import static org.aspectj.weaver.tools.cache.SimpleCacheFactory.path;
@@ -50,11 +52,57 @@ public class RefreshTokenFilter extends OncePerRequestFilter {
         log.info("accessToken : "+accessToken);
         log.info("refreshToken : "+refreshToken);
 
-        try {
+        try { //acceessToken 만료시에는 RefreshTokenExceprion이 전달되지 않아요
             checkAccessToken(accessToken);
         }catch (RefreshTokenException refreshTokenException){
             refreshTokenException.sendResponseError(response);
             return;
+        }
+        Map<String, Object>refreshClaims = null;
+
+        try {
+            refreshClaims = checkRefreshToken(refreshToken);
+            log.info(refreshClaims);
+
+            //새로운 Access Token 발행 ...
+            //1.여기까지 진행되면 무조건AccessToken 발행...
+            //2.refreshToken 만료시간 일이 얼마 남지 않은 경우 새로 발행...
+
+            //Refresh Token의 유효시간이 얼마 남지 않은경우 ......
+            Long exp = (Long) refreshClaims.get("exp");
+
+            Date expTime = new Date(Instant.ofEpochMilli(exp).toEpochMilli() * 1000);
+            Date current = new Date(System.currentTimeMillis());
+
+            //만료 시간과 현재시간의 간격을 계산한다. 3일정도 남았을때 refresh와 AccessToken 둘다 발행하겠다는
+            //정책을 세운다 . 만일 3일 이내라면 refresh토큰을 다시 발행한다.
+
+            long gapTime = (expTime.getTime() - current.getTime());
+            log.info("----------------------------------");
+            log.info("current :  "+current);
+            log.info("expTime : "+expTime);
+            log.info("gap : "+ gapTime);
+
+            String mid = (String) refreshClaims.get("mid");
+
+
+            //여기까지 도착하면 무조건 Token 생성...
+            String accessTokenValue = jwtUtil.generateToken(Map.of("mid",mid),1);
+            String refreshTokenValue = tokens.get("refreshToken"); // 기존에 있던 refreshToken 값
+
+            //기존에 있던 토큰값의 만료 시간이 3일 이내라면 다시 만들어서 집어 넣겠다는 의미다
+            if(gapTime < (1000 * 60 * 60 * 24 * 3)){
+                log.info("new Refresh Token required...");
+                refreshTokenValue = jwtUtil.generateToken(Map.of("mid",mid),1);
+            }
+            log.info("Refresh Token result -----------");
+            log.info("accessToken : "+accessToken);
+            log.info("refreshToken  L" + refreshToken);
+
+
+        }catch (RefreshTokenException refreshTokenException){
+            refreshTokenException.sendResponseError(response);
+            return;//더이상 실행 x
         }
     }
 
